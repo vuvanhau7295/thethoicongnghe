e32178788919ed138a1f231341773ec360d9591c
= <=>
2018-04-15 08:32:54 +0700

    [add] 6 image in project

--- resources/views/news/pages/contact.blade.php
+++ resources/views/news/pages/contact.blade.php
@@ -5,6 +5,8 @@
 	<article class="post column">
 	<h1 class="post-title">Get in touch with us</h1>
 	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ultrices elementum odio, ac fermentum justo sodales vel. Suspendisse potenti. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam scelerisque, massa quis pulvinar accumsan, leo sem iaculis enim, feugiat elementum erat lacus id eros. Nam at nunc metus, sit amet lobortis sem. Etiam ut mauris quis magna condimentum porttitor. Duis sit amet erat porttitor erat dictum molestie quis sit amet mi. Nullam risus massa, euismod id venenatis ut, tempus et eros.</p>
+	<h1 class="post-title">Vũ Văn Hậu</h1>
+	<p>Posts and Telecommunications Institute of Technology</p>
 	<!-- Map -->
 	<div id="map" class="row flex-video widescreen"></div>
 	<!-- End Map -->
